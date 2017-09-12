ab995d6c67c2c748207cb6b5caf1ba3fbafaa36c
Christine Wasike <christinewasike22@gmail.com>
2017-09-12 09:24:53 +0300

    Created the adapter to act as the bridge between my data in my string arrays and the UI of the RestaurantActivity

--- app/src/main/java/com/example/wasike/mymusic/GenreAdapter.java
+++ app/src/main/java/com/example/wasike/mymusic/GenreAdapter.java
@@ -1,8 +1,54 @@
 package com.example.wasike.mymusic;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
 /**
  * Created by wasike on 11/09/17.
  */
 
-public class GenreAdapter {
+public class GenreAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mGenres;
+    public GenreAdapter (Context context, String[] genres){
+        this.mContext = context;
+        this.mGenres = genres;
+    }
+
+
+    @Override
+    public int getCount() {
+        return mGenres.length;
+    }
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gridView;
+        if (convertView == null) {
+            // get layout from xml file
+            gridView = inflater.inflate(R.layout.genre_grid_item, null);
+            // pull views
+            TextView letterView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+            // set values into views
+            letterView.setText("Genre");  // using dummy data for now
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
 }
+
