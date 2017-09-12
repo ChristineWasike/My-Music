19d13fac1ef370d40daeaf1b52c716f02e392744
Christine Wasike <christinewasike22@gmail.com>
2017-09-12 09:26:53 +0300

    Created the basegrid to hold the different grid items containing the music genres

--- app/src/main/java/com/example/wasike/mymusic/MainActivity.java
+++ app/src/main/java/com/example/wasike/mymusic/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.wasike.mymusic;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.onClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //refactoring code using bind to prevent unnecessary repetition
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Bind(R.id.findLyricsButton)
     Button mFindLyricsButton;
-    @Bind(R.id.TitleEditText) Button mTitleEditText;
+    @Bind(R.id.TitleEditText)
+    EditText mTitleEditText;
+    @Bind(R.id.headerTextView)
+    TextView mTextView;
+
+    //implementing the GridView
+    String[] genres = new String[] {"Pop", "Jazz", "Opera",
+            "Blues", "Rock", "Punk", "Dub", "Ska",
+            "Funk", "House", "EDM", "Soul", "Art"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,18 @@ public class MainActivity extends AppCompatActivity implements View.onClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Typeface openFont = Typeface.createFromAsset(getAssets(),"fonts/OpenSans-Regular.ttf");
+        mTextView.setTypeface(openFont);
+
         mFindLyricsButton.setOnClickListener(this);
+
+        GridView gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new GenreAdapter(this, genres));
+    }
+
+    public void toasteMe(View view) {
+        Toast myToast = Toast.makeText(this, "Searching!", Toast.LENGTH_SHORT);
+        myToast.show();
 
     }
 
@@ -30,13 +55,10 @@ public class MainActivity extends AppCompatActivity implements View.onClickListe
     public void onClick(View v) {
         if (v == mFindLyricsButton) {
             String title = mTitleEditText.getText().toString();
-            Intent intent = new Intent(MainActivity.this, Lyr);
+            Log.d(TAG, title);
+            Intent intent = new Intent(MainActivity.this, LyricsActivity.class);
+            intent.putExtra("title", title);
+            startActivity(intent);
         }
-    }
-
-
-    public void toasteMe(View view) {
-        Toast myToast = Toast.makeText(this, "Searching!", Toast.LENGTH_SHORT);
-        myToast.show();
     }
 }
