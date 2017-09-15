d21d173c08c4a034393864e08b43d6098c38c831
Christine Wasike <christinewasike22@gmail.com>
2017-09-15 14:07:52 +0300

    Created new file to store constants

---
+++ app/src/main/java/com/example/wasike/mymusic/Constants.java
@@ -0,0 +1,16 @@
+package com.example.wasike.mymusic;
+
+/**
+ * Created by wasike on 15/09/17.
+ */
+
+public class Constants {
+    public static final String MUSIC_API_KEY = BuildConfig.MUSIC_API_KEY;
+    public static final String MUSIC_BASE_URL = "https://api.musixmatch.com/ws/1.1/track.search?q";
+    public static final String MOVIE_QUERY_PARAMETER = "track.lyrics.get?";
+    public static final String MOVIE_QUERY_PARAMETER2 = "format=jsonp&callback=callback&";
+    public static final String API_KEY_HOLDER = "apikey";
+//    public static final String TRACK_ID =
+    public static final String TRACK_ID_HOLDER = "track_id";
+
+}
