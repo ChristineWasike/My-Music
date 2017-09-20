f5dffc52f86b73bbdc07ea8f9fc1c9fa4a6c4c30
Christine Wasike <christinewasike22@gmail.com>
2017-09-20 15:20:06 +0300

    Created a new class/ Activity to display a users favourite songs

---
+++ app/src/main/java/com/example/wasike/mymusic/adapters/FirebaseSongViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.wasike.mymusic.adapters;
+
+/**
+ * Created by wasike on 19/09/17.
+ */
+
+public class FirebaseSongViewHolder {
+}
---
+++ app/src/main/java/com/example/wasike/mymusic/ui/SavedSongListActivity.java
@@ -0,0 +1,15 @@
+package com.example.wasike.mymusic.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wasike.musicapp.R;
+
+public class SavedSongListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_song_list);
+    }
+}
