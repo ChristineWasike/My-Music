aa27078deb88a87d02c3a91ce8a9534cfa66d3ae
Christine Wasike <christinewasike22@gmail.com>
2017-09-21 12:36:07 +0300

    Created an adapter for my song detail fragment

---
+++ app/src/main/java/com/example/wasike/mymusic/adapters/SongPagerAdapter.java
@@ -0,0 +1,29 @@
+//package com.example.wasike.mymusic.adapters;
+//
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+//
+//import com.example.wasike.mymusic.model.Song;
+//
+//import java.util.ArrayList;
+//
+///**
+// * Created by wasike on 18/09/17.
+// */
+//
+//public class SongPagerAdapter extends FragmentPagerAdapter{
+//    private ArrayList<Song> mSongs;
+//
+//    public SongPagerAdapter(FragmentManager fm, ArrayList<>)
+//
+//    @Override
+//    public Fragment getItem(int position) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return 0;
+//    }
+//}
