4de1cdd8bbb6eb708ead885f56e6f90dcf36e3e2
Christine Wasike <christinewasike22@gmail.com>
2017-09-21 12:36:44 +0300

    Created a model directory for my Song model

---
+++ app/src/main/java/com/example/wasike/mymusic/model/Song.java
@@ -0,0 +1,60 @@
+package com.example.wasike.mymusic.model;
+
+import org.parceler.Parcel;
+
+/**
+ * Created by wasike on 16/09/17.
+ */
+
+@Parcel
+public class Song {
+    private String mType;
+    private String mTitle;
+    private String mUrl;
+    private String mTitleWithFeatured;
+    private String mFulltitle;
+    private String mHeaderImageUrl;
+    private String mArtistName;
+
+
+    public Song(){}
+    public Song(String type, String title, String titleWithFeatured,
+                String url, String fulltitle, String headerImageUrl, String artistName ){
+        mType= type;
+        mTitle = title;
+        mTitleWithFeatured = titleWithFeatured;
+        mUrl = url;
+        mFulltitle = fulltitle;
+        mHeaderImageUrl = headerImageUrl;
+        mArtistName = artistName;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public String getTitleWithFeatured() {
+        return mTitleWithFeatured;
+    }
+
+
+    public String getFulltitle() {
+        return mFulltitle;
+    }
+
+    public String getHeaderImageUrl() {
+        return mHeaderImageUrl;
+    }
+
+    public String getArtistName() {
+        return mArtistName;
+    }
+}
