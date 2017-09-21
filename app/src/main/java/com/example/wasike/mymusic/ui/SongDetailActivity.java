429d8cd8f5e5b5112bf689b2936895d37809198d
Christine Wasike <christinewasike22@gmail.com>
2017-09-21 12:41:24 +0300

    Created activity to house my song detail fragment

---
+++ app/src/main/java/com/example/wasike/mymusic/ui/SongDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.wasike.mymusic.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wasike.mymusic.R;
+
+public class SongDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
