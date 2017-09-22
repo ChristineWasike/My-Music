06dc31f416b75fd9eafcf055ab40d4c34c6eefe5
Christine Wasike <christinewasike22@gmail.com>
2017-09-22 11:41:54 +0300

    Created an adapter that links backened of individual songs to the fragment ui, that's within the viewPager

--- app/src/main/java/com/example/wasike/mymusic/adapters/SongPagerAdapter.java
+++ app/src/main/java/com/example/wasike/mymusic/adapters/SongPagerAdapter.java
@@ -1,29 +1,34 @@
-//package com.example.wasike.mymusic.adapters;
-//
-//import android.support.v4.app.Fragment;
-//import android.support.v4.app.FragmentManager;
+package com.example.wasike.mymusic.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 //import android.support.v4.app.FragmentPagerAdapter;
-//
-//import com.example.wasike.mymusic.model.Song;
-//
-//import java.util.ArrayList;
-//
-///**
+
+import com.example.wasike.mymusic.model.Song;
+import com.example.wasike.mymusic.ui.BlankFragment;
+
+import java.util.ArrayList;
+
+/**
 // * Created by wasike on 18/09/17.
 // */
-//
-//public class SongPagerAdapter extends FragmentPagerAdapter{
-//    private ArrayList<Song> mSongs;
-//
-//    public SongPagerAdapter(FragmentManager fm, ArrayList<>)
-//
-//    @Override
-//    public Fragment getItem(int position) {
-//        return null;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return 0;
-//    }
-//}
+
+public class SongPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Song> mSongs;
+
+    public SongPagerAdapter(FragmentManager fm, ArrayList<Song> songs){
+        super(fm);
+        mSongs = songs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return BlankFragment.newInstance(mSongs.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mSongs.size();
+    }
+}
