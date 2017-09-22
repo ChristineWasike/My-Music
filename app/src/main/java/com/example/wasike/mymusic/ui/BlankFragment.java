0d6c420023ed650159f8704b08fb29a6cd19722c
Christine Wasike <christinewasike22@gmail.com>
2017-09-22 11:46:26 +0300

    Trying to find a way to change the song title on the action bar found in the SongDetail activity

--- app/src/main/java/com/example/wasike/mymusic/ui/BlankFragment.java
+++ app/src/main/java/com/example/wasike/mymusic/ui/BlankFragment.java
@@ -12,6 +12,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.wasike.mymusic.R;
+import com.example.wasike.mymusic.model.Song;
+import com.skyfishjy.library.RippleBackground;
+import com.squareup.picasso.Picasso;
 
 import static android.R.attr.fragment;
 
@@ -24,23 +27,72 @@ import butterknife.ButterKnife;
  * A simple {@link Fragment} subclass.
 
  */
-public class BlankFragment extends Fragment implements View.OnClickListener {
+ public class BlankFragment extends Fragment implements View.OnClickListener {
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 300;
+
+    //@Bind(R.id.content) RippleBackground mRippleBackground;
+
+
+
 
     @Bind(R.id.songImageView) ImageView mImageLabel;
     @Bind(R.id.songNameTextView) TextView mNameLabel;
-    @Bind(R.id.TitleFTTextView) TextView mTitleFTLabel;
-    @Bind(R.id.ratingTextView) TextView mRatingLabel;
+//    @Bind(R.id.TitleFTTextView) TextView mTitleFTLabel;
+    @Bind(R.id.artistTextView) TextView mArtistLabel;
 
-    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
+    @Bind(R.id.centerImage) ImageView mWebsiteLabel;
     @Bind(R.id.FTArtistTextView) TextView mFTArtistLabel;
-//    @Bind(R.id.);
+
     @Bind(R.id.saveSongButton) TextView mSaveSongButton;
+
+     private Song mSong;
+
+    //created the fragment to display information on an individual song
+    public  static  BlankFragment newInstance(Song song) {
+      BlankFragment songDetailFragment = new BlankFragment();
+      Bundle args = new Bundle();
+      args.putParcelable("song", Parcels.wrap(song));
+      songDetailFragment.setArguments(args);
+      return songDetailFragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+       // final RippleBackground rippleBackground = (RippleBackground) mRippleBackground;
         super.onCreate(savedInstanceState);
+        mSong = Parcels.unwrap(getArguments().getParcelable("song"));
+       // rippleBackground.startRippleAnimation();
+
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mSong.getArtistImage()).resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop().into(mImageLabel);
+
+        mNameLabel.setText(mSong.getTitle());
+        mArtistLabel.setText(mSong.getArtistName());
+        mFTArtistLabel.setText(mSong.getTitleWithFeatured());
+
+        mSaveSongButton.setOnClickListener(this);
+
+        mWebsiteLabel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //rippleBackground.startRippleAnimation();
+                Intent intent = new Intent(view.getContext(), LyricsWebViewActivity.class);
+                intent.putExtra("url", mSong.getUrl());
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
+    }
 
     @Override
     public void onClick(View view) {
