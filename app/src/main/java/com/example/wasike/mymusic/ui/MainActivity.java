eb0b7d515e12c0db3f4d036a1342879c53352da4
Christine Wasike <christinewasike22@gmail.com>
2017-09-25 17:01:29 +0300

    Implemented the haulting of the progress bar

--- app/src/main/java/com/example/wasike/mymusic/ui/MainActivity.java
+++ app/src/main/java/com/example/wasike/mymusic/ui/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.wasike.mymusic.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,17 +15,20 @@ import android.widget.Toast;
 
 import com.example.wasike.mymusic.R;
 import com.example.wasike.mymusic.ui.MTActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static String TAG = MainActivity.class.getSimpleName();
-    @Bind(R.id.SongEditText)EditText mSongEditText;
     @Bind(R.id.Songbutton) Button mSongButton;
+    @Bind(R.id.savedSongbutton) Button mSavedSongButton;
 
 
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         mSongButton.setOnClickListener(this);
+
+        mSavedSongButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                //display welcome message
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, "+ user.getDisplayName() + "!");
+                } else {
+                    getSupportActionBar().setTitle("Welcome, " + "stranger!");
+
+                }
+            }
+        };
     }
 
     public void toasteMe(View view) {
@@ -41,11 +66,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if (view == mSongButton) {
-            String title = mSongEditText.getText().toString();
-            Log.d(TAG, title);
             Intent intent = new Intent(this, MTActivity.class);
-            intent.putExtra("title", title);
+            Toast myToast = Toast.makeText(this, "Searching!", Toast.LENGTH_SHORT);
+            myToast.show();
             startActivity(intent);
         }
+
+        if (view == mSavedSongButton) {
+            Intent intent = new Intent(MainActivity.this, SavedSongListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public  void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/wasike/mymusic/ui/SavedSongListActivity.java
+++ app/src/main/java/com/example/wasike/mymusic/ui/SavedSongListActivity.java
@@ -2,14 +2,68 @@ package com.example.wasike.mymusic.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.example.wasike.mymusic.Constants;
 import com.example.wasike.mymusic.R;
+import com.example.wasike.mymusic.adapters.FirebaseSongViewHolder;
+import com.example.wasike.mymusic.model.Song;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedSongListActivity extends AppCompatActivity {
+    private DatabaseReference mSongReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.progressBar) ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_song_list);
+        setContentView(R.layout.activity_mt);
+        ButterKnife.bind(this);
+
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mSongReference = FirebaseDatabase.getInstance()
+                .getReference(Constants.FIREBASE_CHILD_SONGS)
+                .child(uid);
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Song, FirebaseSongViewHolder>
+                (Song.class, R.layout.song_list_item_drag, FirebaseSongViewHolder.class,
+                        mSongReference) {
+            @Override
+            protected void populateViewHolder(FirebaseSongViewHolder viewHolder, Song model, int position) {
+                viewHolder.bindSong(model);
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
