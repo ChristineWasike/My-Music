4e7fcb96165343246e2c27d33f5d2675d59cc49f
Christine Wasike <christinewasike22@gmail.com>
2017-09-25 16:13:35 +0300

    Added the firebase child song and child search song constants

--- app/src/main/java/com/example/wasike/mymusic/Constants.java
+++ app/src/main/java/com/example/wasike/mymusic/Constants.java
@@ -10,4 +10,7 @@ public class Constants {
     public static final String MUSIC_API_KEY = BuildConfig.MUSIC_API_KEY;
     public static final String MUSIC_BASE_URL = "https://api.genius.com/search?";
     public static final String MUSIC_QUERY_PARAMETER = "q";
+    public static final String PREFERENCE_SONG_KEY = "song";
+    public static final String FIREBASE_CHILD_SEARCHED_SONG = "search";
+    public static final String FIREBASE_CHILD_SONGS = "songs";
 }
