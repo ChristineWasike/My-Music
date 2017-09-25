2c3b7233ba7d3530383ab7f82ee6bb8ac8e60249
Christine Wasike <christinewasike22@gmail.com>
2017-09-25 17:02:07 +0300

    Implemented saving song to specific users

--- app/src/main/java/com/example/wasike/mymusic/util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/example/wasike/mymusic/util/ItemTouchHelperAdapter.java
@@ -5,4 +5,6 @@ package com.example.wasike.mymusic.util;
  */
 
 public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
 }
--- app/src/main/java/com/example/wasike/mymusic/util/OnStartDragListener.java
+++ app/src/main/java/com/example/wasike/mymusic/util/OnStartDragListener.java
@@ -1,8 +1,11 @@
 package com.example.wasike.mymusic.util;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by wasike on 25/09/17.
  */
 
 public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
