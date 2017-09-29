59998ac4b4db5938593ae30d4c771dfbf5d6dc11
Christine Wasike <christinewasike22@gmail.com>
2017-09-29 12:46:10 +0300

    Implemented the ChildEventListeners to my app

--- app/src/main/java/com/example/wasike/mymusic/adapters/FirebaseSongListAdapter.java
+++ app/src/main/java/com/example/wasike/mymusic/adapters/FirebaseSongListAdapter.java
@@ -1,13 +1,23 @@
 package com.example.wasike.mymusic.adapters;
 
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.example.wasike.mymusic.model.Song;
 import com.example.wasike.mymusic.util.ItemTouchHelperAdapter;
 import com.example.wasike.mymusic.util.OnStartDragListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 /**
  * Created by wasike on 25/09/17.
@@ -18,26 +28,69 @@ public class FirebaseSongListAdapter extends FirebaseRecyclerAdapter<Song, Fireb
     private OnStartDragListener mOnStartDragListener;
     private Context mContext;
 
+    private ChildEventListener mChildEventListener;
+    private ArrayList<Song> mSongs = new ArrayList<>();
+
+
     public FirebaseSongListAdapter(Class<Song> modelClass, int modelLayout, Class<FirebaseSongViewHolder> viewHolderClass,
                                    Query ref,OnStartDragListener onStartDragListener, Context context) {
         super(modelClass, modelLayout, viewHolderClass, ref);
         mRef = ref.getRef();
         mOnStartDragListener = onStartDragListener;
         mContext = context;
+
+        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mSongs.add(dataSnapshot.getValue(Song.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
-    protected void populateViewHolder(FirebaseSongViewHolder viewHolder, Song model, int position) {
+    protected void populateViewHolder(final FirebaseSongViewHolder viewHolder, Song model, int position) {
     viewHolder.bindSong(model);
+        viewHolder.mSongImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    mOnStartDragListener.onStartDrag(viewHolder);
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
+
 
     }
 
