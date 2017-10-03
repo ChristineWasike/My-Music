e835641c21a68543dbe1cc6a61a72e06f6b78a68
Christine Wasike <christinewasike22@gmail.com>
2017-10-03 08:04:12 +0300

    Added the song api path as a constant for retrieving info on a specified song

--- app/src/main/java/com/example/wasike/mymusic/Constants.java
+++ app/src/main/java/com/example/wasike/mymusic/Constants.java
@@ -1,6 +1,9 @@
 package com.example.wasike.mymusic;
 
+import android.util.Log;
+
 import com.example.wasike.mymusic.BuildConfig;
+import com.example.wasike.mymusic.model.Song;
 
 /**
  * Created by wasike on 16/09/17.
@@ -10,6 +13,7 @@ public class Constants {
     public static final String MUSIC_API_KEY = BuildConfig.MUSIC_API_KEY;
     public static final String MUSIC_BASE_URL = "https://api.genius.com/search?";
     public static final String MUSIC_QUERY_PARAMETER = "q";
+    public static final String SONG_BASE_URL = "https://api.genius.com/";
     public static final String PREFERENCE_SONG_KEY = "song";
     public static final String FIREBASE_CHILD_SEARCHED_SONG = "search";
     public static final String FIREBASE_CHILD_SONGS = "songs";
