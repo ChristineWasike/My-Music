2ac507dbee7d70a54e6392fc00fdc5f3cdd20c78
Christine Wasike <christinewasike22@gmail.com>
2017-10-03 08:12:36 +0300

    Created a splas screen that is launched before the app loads up

--- app/src/main/java/com/example/wasike/mymusic/ui/Splash.java
+++ app/src/main/java/com/example/wasike/mymusic/ui/Splash.java
@@ -1,15 +1,29 @@
 package com.example.wasike.mymusic.ui;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.wasike.mymusic.R;
+import com.example.wasike.mymusic.R;
 
 public class Splash extends AppCompatActivity {
+    public static int time_out = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, time_out);
+
+        getSupportActionBar().hide();
     }
 }
