00fb2609cb7a8738b9de7b99fd655eb8efeea6e6
Christine Wasike <christinewasike22@gmail.com>
2017-10-05 08:43:29 +0300

    Created adapter responsible for populating the list items with data on song title and name of artist

--- app/src/main/java/com/example/wasike/mymusic/adapters/PhoneAdapter.java
+++ app/src/main/java/com/example/wasike/mymusic/adapters/PhoneAdapter.java
@@ -1,8 +1,70 @@
 package com.example.wasike.mymusic.adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.wasike.mymusic.R;
+import com.example.wasike.mymusic.model.Phone;
+
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  * Created by wasike on 04/10/17.
  */
 
-public class PhoneAdapter {
+public class PhoneAdapter extends BaseAdapter {
+    private ArrayList<Phone> phoneSongs;
+    private LayoutInflater songInf;
+    private  Phone mPhone;
+    private Context mContext;
+
+    public PhoneAdapter(Context c, ArrayList<Phone> theSongs) {
+        phoneSongs = theSongs;
+        songInf = LayoutInflater.from(c);
+    }
+
+
+    @Override
+    public int getCount() {
+        return phoneSongs.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //map to song layout
+        LinearLayout songLay = (LinearLayout)songInf.inflate(R.layout.song, parent, false);
+        //get title and artist views
+        TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        //get song using position
+        Phone currSong = phoneSongs.get(position);
+        //get title and artist strings
+        songView.setText(currSong.getphoneTitle());
+        artistView.setText(currSong.getphoneArtist());
+        //set position as tag
+        songLay.setTag(position);
+
+
+
+        return songLay;
+    }
 }
