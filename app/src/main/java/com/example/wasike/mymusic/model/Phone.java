c3e3cd2a813ba10f759144c3afd0a199b1fc0843
Christine Wasike <christinewasike22@gmail.com>
2017-10-05 08:48:28 +0300

    Created a service that helps retrieve information on aa song fro respective android devices

--- app/src/main/java/com/example/wasike/mymusic/model/Phone.java
+++ app/src/main/java/com/example/wasike/mymusic/model/Phone.java
@@ -1,8 +1,31 @@
 package com.example.wasike.mymusic.model;
 
+import static android.R.attr.id;
+
 /**
  * Created by wasike on 04/10/17.
  */
 
 public class Phone {
+    private long phoneId;
+    private String phoneTitle;
+    private String phoneArtist;
+
+    public Phone(long songID, String songTitle, String songArtist) {
+        phoneId=songID;
+        phoneTitle=songTitle;
+        phoneArtist=songArtist;
+    }
+
+    public long getphoneID(){
+        return phoneId;
+    }
+
+    public String getphoneTitle(){
+        return phoneTitle;
+    }
+
+    public String getphoneArtist(){
+        return phoneArtist;
+    }
 }
--- app/src/main/java/com/example/wasike/mymusic/services/PhoneService.java
+++ app/src/main/java/com/example/wasike/mymusic/services/PhoneService.java
@@ -1,8 +1,18 @@
 package com.example.wasike.mymusic.services;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by wasike on 04/10/17.
  */
 
-public class PhoneService {
+public class PhoneService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
