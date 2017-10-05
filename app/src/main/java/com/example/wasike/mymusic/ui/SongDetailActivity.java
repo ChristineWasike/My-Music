6cb07f675de026a573907363e73341f75bc0012a
Christine Wasike <christinewasike22@gmail.com>
2017-10-05 09:34:24 +0300

    Worked on the outlook of each song and borrowed it for the song list item drag

--- app/src/main/java/com/example/wasike/mymusic/ui/SongDetailActivity.java
+++ app/src/main/java/com/example/wasike/mymusic/ui/SongDetailActivity.java
@@ -1,29 +1,21 @@
 package com.example.wasike.mymusic.ui;
 
-import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.RotateAnimation;
 
 import com.ToxicBakery.viewpager.transforms.CubeOutTransformer;
 import com.example.wasike.mymusic.R;
 import com.example.wasike.mymusic.adapters.SongPagerAdapter;
 import com.example.wasike.mymusic.model.Song;
-
 import org.parceler.Parcels;
-
 import java.util.ArrayList;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SongDetailActivity extends AppCompatActivity {
     @Bind(R.id.viewPager) ViewPager mViewPager;
-    private SongPagerAdapter adapterViewPager;
     ArrayList<Song> mSongs = new ArrayList<>();
-    private Song mSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +26,7 @@ public class SongDetailActivity extends AppCompatActivity {
         mSongs = Parcels.unwrap(getIntent().getParcelableExtra("songs"));
         int startingPosition = getIntent().getIntExtra("position", 0);
 
-        adapterViewPager = new SongPagerAdapter(getSupportFragmentManager(), mSongs);
+        SongPagerAdapter adapterViewPager = new SongPagerAdapter(getSupportFragmentManager(), mSongs);
         mViewPager.setAdapter(adapterViewPager);
         mViewPager.setPageTransformer(true, new CubeOutTransformer());
         mViewPager.setCurrentItem(startingPosition);
