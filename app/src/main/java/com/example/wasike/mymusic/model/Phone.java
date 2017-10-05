0bacc0ab7d70b3cfd8d06f03f3911970ebf75e69
Christine Wasike <christinewasike22@gmail.com>
2017-10-05 08:40:04 +0300

    Added the Splash screen with a dj iage sonn to have an animation

---
+++ app/src/main/java/com/example/wasike/mymusic/model/Phone.java
@@ -0,0 +1,8 @@
+package com.example.wasike.mymusic.model;
+
+/**
+ * Created by wasike on 04/10/17.
+ */
+
+public class Phone {
+}
---
+++ app/src/main/java/com/example/wasike/mymusic/ui/PhoneStore.java
@@ -0,0 +1,15 @@
+package com.example.wasike.mymusic.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wasike.mymusic.R;
+
+public class PhoneStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_store);
+    }
+}
